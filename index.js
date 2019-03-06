2639f626ad9ed5f607c496082395d8d0d1a02b4c
Saint-Jean Germila <germila@noukod.com>
2019-03-06 15:10:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver (name) {
 }
 
 function appendDriver(name) {
-  const appendDriver = [...drivers, "Malia"];
+  const appendDriver = [...drivers, "name"];
   drivers;
 }
