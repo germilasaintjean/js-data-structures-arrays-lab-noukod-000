722b6d6bca258a37c41cbb138b4b9665fc9a671e
Saint-Jean Germila <germila@noukod.com>
2019-03-06 14:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
+
+function logdestructivelyAppendDriver (name) {
+  console.log("drivers:", name);
+}
+logdestructivelyAppendDriver(drivers);
